7851c637535fccf3553e4695808733afe8e24682
QuickFox43 <koalal1428@gmail.com>
2025-06-04 15:26:05 +0300

    refactor(di): remove global redis and db instances

--- backend/src/core/di/providers/redis.py
+++ backend/src/core/di/providers/redis.py
@@ -3,13 +3,18 @@ from collections.abc import AsyncIterator
 from dishka import Provider, Scope, provide
 from redis.asyncio import Redis
 
-from src.core.redis import redis_manager
+from src.core.config import RedisConfig
+from src.core.redis import RedisManager
 
 
 class RedisProvider(Provider):
     scope = Scope.APP
 
     @provide
-    async def get_redis_client(self) -> AsyncIterator[Redis]:
-        async with redis_manager as redis:
+    def get_redis_manager(self, config: RedisConfig) -> RedisManager:
+        return RedisManager(config.host, str(config.port))
+
+    @provide
+    async def get_redis_client(self, manager: RedisManager) -> AsyncIterator[Redis]:
+        async with manager as redis:
             yield redis
